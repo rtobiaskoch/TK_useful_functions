6e161058b2d08ad289a4eeb681c4cbb5d5106a74
Robert Koch <r.tobiaskoch@gmail.com>
2024-10-16 19:13:05 -0600

    updated gsheet_pull

--- gsheet_pull.R
+++ gsheet_pull.R
@@ -1,8 +1,7 @@
 gsheet_pull = function(key, sheet, out_fn) {
   require(googlesheets4)
   library(googlesheets4)
-  
-  googlesheets4::gs4_auth()
+
   googlesheets4::read_sheet(ss = key, sheet = sheet)
 
   mdata = googlesheets4::read_sheet(key, sheet = sheet)
