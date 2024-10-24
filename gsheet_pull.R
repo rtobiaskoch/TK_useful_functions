230d9ef8fd73d84f10847b4a9329e8d5f93fb85f
Robert Koch <r.tobiaskoch@gmail.com>
2024-10-24 14:49:37 -0600

    updated gsheet_read_run.R

--- gsheet_pull.R
+++ gsheet_pull.R
@@ -1,15 +1,22 @@
-gsheet_pull = function(key, sheet, out_fn) {
+
+
+gsheet_pull = function(key, sheet, out_fn, update = T) {
+  
   require(googlesheets4)
   library(googlesheets4)
-
-  googlesheets4::read_sheet(ss = key, sheet = sheet)
-
-  mdata = googlesheets4::read_sheet(key, sheet = sheet)
-    
-    #authorize token response
-    # 1
-    
+  
+  if(update == T) { #if update is T read in new 
+    googlesheets4::gs4_auth()
+    mdata = read_sheet(key, sheet = sheet)
     #mdata = apply(mdata, 2, as.character) #added because error   unimplemented type 'list' in 'EncodeElement' was occuring
     write.csv(mdata, out_fn, row.names = F, na = "")
+    read.csv(out_fn)
+  } else { # if you don't want to pull it off google drive load the out_fn locally
+    if(file.exists(out_fn)) {read.csv(out_fn)} 
+    else {
+      print(paste(out_fn, "file doesn't exist, please set update = T or download desired gsheet file"))
+    }
     
-  }   
+  }
+  
+}   
